23819109ec01c9ef95092d72ff1a990799ba0a62
Emile Greyling <emilegreyling14@gmail.com>
2023-08-30 19:49:12 +0200

    Fixed error when uploading PDF

--- js/main.js
+++ js/main.js
@@ -1,101 +1,105 @@
-const url = '../docs/pdf.pdf'
+const url = "../docs/pdf.pdf";
 
-let pdfDoc = null
-let pageNum = 1
-let pageIsRendering = false
-let pageNumIsPending = null
+let pdfDoc = null;
+let pageNum = 1;
+let pageIsRendering = false;
+let pageNumIsPending = null;
 
-const scale = 1.5
-const canvas = document.querySelector('#pdf-render')
-const ctx = canvas.getContext('2d')
+const scale = 1.5;
+const canvas = document.querySelector("#pdf-render");
+const ctx = canvas.getContext("2d");
 
 // Function to render the page
-const renderPage = num => {
-  pageIsRendering = true
-  
+const renderPage = (num) => {
+  pageIsRendering = true;
+
   // Get page
-  pdfDoc.getPage(num).then(page => {
+  pdfDoc.getPage(num).then((page) => {
     // Set scale
-    const viewport = page.getViewport({ scale })
-    canvas.height = viewport.height
-    canvas.width = viewport.width
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
 
     const renerCtx = {
       canvasContext: ctx,
-      viewport
-    }
+      viewport,
+    };
 
     page.render(renerCtx).promise.then(() => {
-      pageIsRendering = false
+      pageIsRendering = false;
 
-      if(pageNumIsPending !== null) {
-        renderPage(pageNumIsPending)
-        pageNumIsPending = null
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
       }
-    })
+    });
 
     // Output current page
-    document.querySelector('#page-num').textContent = num;
-
-  })
-}
+    document.querySelector("#page-num").textContent = num;
+  });
+};
 
 // Check for pages rendering
-const queueRenderPage = num => {
+const queueRenderPage = (num) => {
   if (pageIsRendering) {
-    pageNumIsPending = num
+    pageNumIsPending = num;
   } else {
-    renderPage(num)
+    renderPage(num);
   }
-}
+};
 
 // Show Prev page
 const showPrevPage = () => {
   if (pageNum <= 1) {
-    return
+    return;
   }
-  pageNum--
-  queueRenderPage(pageNum)
-}
+  pageNum--;
+  queueRenderPage(pageNum);
+};
 
 // Show next page
 const showNextPage = () => {
   if (pageNum >= pdfDoc.numPages) {
-    return
+    return;
   }
-  pageNum++
-  queueRenderPage(pageNum)
-}
+  pageNum++;
+  queueRenderPage(pageNum);
+};
 
 // Function to get the document
 const input = document.getElementById("fileInput");
-input.addEventListener("change", function() {
+input.addEventListener("change", function () {
+  pdfDoc = null;
+  pageNum = 1;
+  pageIsRendering = false;
+  pageNumIsPending = null;
   // Get the selected file as a File object
   const file = input.files[0];
-  
+
   const reader = new FileReader();
-  reader.onload = function() {
+  reader.onload = function () {
     // Get the file content as an ArrayBuffer
     const arrayBuffer = reader.result;
-    pdfjsLib.getDocument(arrayBuffer).promise.then(_pdfDoc => {
-        pdfDoc = _pdfDoc
-        
-        document.querySelector('#page-count').textContent = pdfDoc._pdfInfo.numPages
-      
-        renderPage(pageNum)
-    })
+    pdfjsLib.getDocument(arrayBuffer).promise.then((_pdfDoc) => {
+      pdfDoc = _pdfDoc;
+
+      document.querySelector("#page-count").textContent =
+        pdfDoc._pdfInfo.numPages;
+
+      renderPage(pageNum);
+    });
   };
   reader.readAsArrayBuffer(file);
 });
 
-pdfjsLib.getDocument(url).promise.then(_pdfDoc => {
-  pdfDoc = _pdfDoc
-  
-  document.querySelector('#page-count').textContent = pdfDoc._pdfInfo.numPages
+pdfjsLib.getDocument(url).promise.then((_pdfDoc) => {
+  pdfDoc = _pdfDoc;
 
-  renderPage(pageNum)
-})
+  document.querySelector("#page-count").textContent = pdfDoc._pdfInfo.numPages;
+
+  renderPage(pageNum);
+});
 
 // Button Events
-document.querySelector('#prev-page').addEventListener('click', showPrevPage)
-document.querySelector('#next-page').addEventListener('click', showNextPage)
+document.querySelector("#prev-page").addEventListener("click", showPrevPage);
+document.querySelector("#next-page").addEventListener("click", showNextPage);
